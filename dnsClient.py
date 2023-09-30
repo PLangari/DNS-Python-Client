3a184027d263f2a0f08901f3207a4fe0a3fffb25
Parsa Langari <parsa.langari@mail.mcgill.ca>
2023-09-30 18:30:09 -0400

    modularization functions

--- dnsClient.py
+++ dnsClient.py
@@ -2,6 +2,49 @@ import socket
 import argparse
 import struct
 
+
+def parse_dns_response(response):
+    # Unpacking header
+    transaction_id, flags, questions, answer_rrs, ns_count, ar_count = struct.unpack(
+        "!HHHHHH", response[:12]
+    )
+
+    print(f"Transaction ID: {transaction_id}")
+    print(f"Flags: {flags}")
+    print(f"Questions: {questions}")
+    print(f"Answer RRs: {answer_rrs}")
+    print(f"Authority RRs: {ns_count}")
+    print(f"Additional RRs: {ar_count}")
+
+    offset = 12
+
+    # Skipping the question section, just for simplicity
+    for _ in range(questions):
+        while response[offset] != 0:
+            label_len = response[offset]
+            offset += label_len + 1
+        offset += 5  # QTYPE + QCLASS
+
+    print("Answers:")
+    for _ in range(answer_rrs):
+        if offset + 12 > len(response):  # To ensure we have the full record header
+            print("Incomplete record. Exiting.")
+            break
+
+        name_ptr, res_type, res_class, ttl, rd_length = struct.unpack(
+            "!HHHLH", response[offset : offset + 12]
+        )
+        offset += 10
+
+        if offset + rd_length > len(response):
+            print("Incomplete record data. Exiting.")
+            break
+
+        rdata = response[offset : offset + rd_length]
+        print(f"Type: {res_type}, TTL: {ttl}, Data: {rdata}")
+        offset += rd_length
+
+
 def create_dns_query(domain, query_type="A"):
     """
     Create a DNS query based on the given domain and type.
@@ -67,69 +110,47 @@ def dns_query(server, port, domain, query_type, timeout, max_retries):
             print("ERROR\tMaximum number of retries exceeded")
             return
 
-        print(response)
+        parse_dns_response(response)
 
-       # Unpacking header
-        transaction_id, flags, questions, answer_rrs, ns_count, ar_count = struct.unpack("!HHHHHH", response[:12])
 
-        print(f"Transaction ID: {transaction_id}")
-        print(f"Flags: {flags}")
-        print(f"Questions: {questions}")
-        print(f"Answer RRs: {answer_rrs}")
-        print(f"Authority RRs: {ns_count}")
-        print(f"Additional RRs: {ar_count}")
+def input_parser():
+    parser = argparse.ArgumentParser(description="A simple DNS client.")
+    parser.add_argument(
+        "-t", "--timeout", type=int, default=5, help="Timeout in seconds"
+    )
+    parser.add_argument(
+        "-r", "--max-retries", type=int, default=3, help="Maximum retries"
+    )
+    parser.add_argument(
+        "-p", "--port", type=int, default=53, help="Port of the DNS server"
+    )
+    parser.add_argument("-mx", action="store_true", help="Query for MX record")
+    parser.add_argument("-ns", action="store_true", help="Query for NS record")
+    parser.add_argument("server", type=str, help="DNS server IP address")
+    parser.add_argument("name", type=str, help="Domain name to query for")
 
-        offset = 12
-
-        # Skipping the question section, just for simplicity
-        for _ in range(questions):
-            while response[offset] != 0:
-                label_len = response[offset]
-                offset += label_len + 1
-            offset += 5  # QTYPE + QCLASS
-
-        print("Answers:")
-        for _ in range(answer_rrs):
-            if offset + 12 > len(response):  # To ensure we have the full record header
-                print("Incomplete record. Exiting.")
-                break
-
-            name_ptr, res_type, res_class, ttl, rd_length = struct.unpack("!HHHLH", response[offset: offset + 12])
-            offset += 10
-
-            if offset + rd_length > len(response):
-                print("Incomplete record data. Exiting.")
-                break
-
-            rdata = response[offset: offset + rd_length]
-            print(f"Type: {res_type}, TTL: {ttl}, Data: {rdata}")
-            offset += rd_length
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='A simple DNS client.')
-    parser.add_argument('-t', '--timeout', type=int, default=5, help='Timeout in seconds')
-    parser.add_argument('-r', '--max-retries', type=int, default=3, help='Maximum retries')
-    parser.add_argument('-p', '--port', type=int, default=53, help='Port of the DNS server')
-    parser.add_argument('-mx', action='store_true', help='Query for MX record')
-    parser.add_argument('-ns', action='store_true', help='Query for NS record')
-    parser.add_argument('server', type=str, help='DNS server IP address')
-    parser.add_argument('name', type=str, help='Domain name to query for')
-    
     args = parser.parse_args()
 
-    query_type = 'A'
+    query_type = "A"
     if args.mx:
-        query_type = 'MX'
+        query_type = "MX"
     elif args.ns:
-        query_type = 'NS'
+        query_type = "NS"
 
-    server_address = args.server.replace('@', '')  # Remove @ if it exists
+    server_address = args.server.replace("@", "")  # Remove @ if it exists
+    port = args.port
+    domain = args.name
+    timeout = args.timeout
+    max_retries = args.max_retries
 
-    print("timeout: ", args.timeout)
-    print("max-retries: ", args.max_retries)
-    print("port: ", args.port)
-    print("query_type: ", query_type)
-    print("server: ", server_address)
-    print("name: ", args.name)
+    return server_address, port, domain, timeout, max_retries, query_type
 
-    dns_query(server_address, args.port, args.name, query_type, args.timeout, args.max_retries)
+
+def main():
+    server_address, port, domain, timeout, max_retries, query_type = input_parser()
+
+    dns_query(server_address, port, domain, query_type, timeout, max_retries)
+
+
+if __name__ == "__main__":
+    main()
